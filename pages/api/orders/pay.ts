379378b118484b313a2c990e42e2b21424955a8b
AlexZama-San <alexRoJas_199@hotmail.com>
2023-01-10 22:43:40 -0600

    implementacion de paypal

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
 
 type Data = {
     message: string
@@ -12,6 +13,34 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 
         default:
             return res.status(405).json({ message: 'Method not allowed' })
+    }
+}
+
+const getPaypalBearerToken = async():Promise<string|null> => {
+
+    const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT
+    const PAYPAL_SECRET = process.env.PAYPAL_SECRET
+
+    const base64Token = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_SECRET}`)
+
+    const body = new URLSearchParams('grant_type=client_credentials')
+    try {
+        const {data} = await axios.post(process.env.PAYPAL_OAUTH_URL || '', body, {
+            headers: {
+                'Authorization': `Basic ${base64Token}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+
+        return data.access_token
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log(error.response?.data)
+        }else{
+            console.log(error)
+        }
+
+        return null
     }
 }
 
